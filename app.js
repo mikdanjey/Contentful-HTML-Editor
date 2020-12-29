5b9da3db09b032d02b1c0feb0b7e06d72b3d8f72
Mikdan Jey <mikdanjey@gmail.com>
2020-12-29 12:45:44 +0530

    updated

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ import { render } from 'react-dom';
 import { init, locations } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss/dist/styles.css';
-import { RichTextEditor, renderRichTextDialog } from '@contentful/field-editor-rich-text';
+import { MultipleLineEditor } from '@contentful/field-editor-multiple-line';
 
 init(sdk => {
   const root = document.getElementById('root');
   sdk.window.startAutoResizer();
-  if (sdk.location.is(locations.LOCATION_DIALOG)) {
-    render(renderRichTextDialog(sdk), root);
-  } else {
-    render(<RichTextEditor sdk={sdk} value={"Mikdan Text"} />, root);
-  }
+  render(<MultipleLineEditor sdk={sdk} value={"Mikdan Text"} />, root);
 });
