54e7745127899ca2499a295987945923a12046c4
Mikdan Jey <mikdanjey@gmail.com>
2020-12-29 20:11:52 +0530

    ok

--- monolithic/app.js
+++ monolithic/app.js
@@ -5,12 +5,15 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, './app.html'));
+});
+
+app.get('/test', (_, res) => {
   res.sendFile(path.join(__dirname, './test.html'));
 });
 
 // local variables
 const PORT = 5000;
-
 app.listen(PORT, error => {
   if (error) {
     console.error(error);
